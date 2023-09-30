cbf9457654a68952764c75674090ae5a8db6e496
Maxime Gagnon <m.gagnon941@gmail.com>
2023-09-30 16:38:02 -0400

    feat: Style chart

--- index.js
+++ index.js
@@ -1,31 +1,68 @@
-const ctx = document.getElementById("productivity-chart")
-const json= fetch("https://run.mocky.io/v3/b4add931-0807-4c21-864c-ce67e16ef236")
-    .then(response => response.json()
-    .then(data => {
-        console.log(data)
-        createWeeklyChart(data)
+const $ = document.querySelector.bind(document)
+const ctx = $("#productivity-chart")
 
-        const spinner = document.getElementById("loading-spinner")
+fetch("https://run.mocky.io/v3/02b569d9-5131-4e3b-a100-7917fe72fcaf")
+    .then(response => response.json())
+    .then(data => {
+        try {
+            createWeeklyChart(data)
+        } catch(e) {
+            alert(e.message)
+        }
+
+        const spinner = $("#loading-spinner")
         spinner.style.opacity = "0"
         setTimeout(() => {
             spinner.style.display = "none"
         }, 500)
-    }))
+    })
 
 function createWeeklyChart(data) {
+    const err = (message) => {
+        throw new Error(`Invalid chart data provided: ${message}.`)
+    }
+
     if (!data)
-        throw new Error("No chart data provided.");
+        err("no data available")
+
+    const results = data.results
+
+    if (!results)
+        err("no metrics found")
 
     new Chart(ctx,{
         type: "line",
         data: {
-            labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            labels: results.map(r => r.day),
             datasets: [{
-                label: "Productivity",
-                data: [69, 40, 60, 81, 56, 55, 40],
-                borderWidth: 1,
+                data: results.map(r => r.averageProductivity * 100),
+                borderWidth: 3,
+                borderColor: "#6d4ea7",
+                lineTension: 0.4,
+                fill: true,
+                backgroundColor: "rgba(109, 78, 167, 0.1)",
             }]
         },
-        options: {}
+        options: {
+            scales: {
+                x: {
+                    ticks: {
+                        padding: 10
+                    }
+                },
+                y: {
+                    min: 0,
+                    max: 100,
+                    ticks: {
+                        callback: (value) => `${value}%`,
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
     });
 }
