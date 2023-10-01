8642df708c0ad840dd9b1a84ab62a31db6d21df7
Maxime Gagnon <m.gagnon941@gmail.com>
2023-10-01 10:55:51 -0400

    feat: Display hours

--- chrome-extension/scripts/chart.mjs
+++ chrome-extension/scripts/chart.mjs
@@ -1,8 +1,9 @@
 import {$} from "./utils.mjs";
 
+let chart;
 const ctx = document.querySelector("#chart")
 
-const chartOptions = (labels, data) => ({
+const chartOptions = (labels, data, tooltipTitle, tooltipLabel) => ({
     type: "line",
     data: {
         labels: labels,
@@ -40,10 +41,8 @@ const chartOptions = (labels, data) => ({
                 xAlign: "center",
                 yAlign: "bottom",
                 callbacks: {
-                    title(t) {
-                        return `Average productivity: ${t[0].raw}%`
-                    },
-                    label: (context) => `Click to inspect day`
+                    title: tooltipTitle,
+                    label: tooltipLabel,
                 }
             }
         }
@@ -53,17 +52,31 @@ const chartOptions = (labels, data) => ({
 function createWeeklyChart(data, week) {
     const labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
     const days = data.weeks[week].days.map(day => day.averageProductivity * 100)
-    const chart = new Chart(ctx, chartOptions(labels, days))
+    const tooltipTitle = (t) => `Average productivity: ${t[0].raw}%`
+    const tooltipLabel = (t) => `Click to see inspect the day`
+    chart = new Chart(ctx, chartOptions(labels, days, tooltipTitle, tooltipLabel))
 
     $("#chart").onclick = (e) => {
         const canvasPosition = Chart.helpers.getRelativePosition(e, chart)
-
-        // Substitute the appropriate scale IDs
         const dataX = chart.scales.x.getValueForPixel(canvasPosition.x)
-        const dataY = chart.scales.y.getValueForPixel(canvasPosition.y)
-
-        alert(dataX)
+        createDailyChart(data, week, dataX)
     }
+}
+
+function createDailyChart(data, week, day) {
+    const labels = [
+        "1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h",
+        "11h", "12h", "13h", "14h", "15h", "16h", "17h", "18h", "19h",
+        "20h", "21h", "22h", "23h", "24h"
+    ]
+    const hours = data.weeks[week].days[day].hours.map(hour => hour.averageProductivity * 100)
+    console.log(data)
+    const websites = data.weeks[week].days[day].hours.map(hour => hour.websites)
+    const tooltipTitle = (t) => `Average productivity: ${t[0].raw}%`
+    const tooltipLabel = (t) => `Website: ${websites[t.dataIndex].url}`
+
+    chart.destroy()
+    chart = new Chart(ctx, chartOptions(labels, hours, tooltipTitle, tooltipLabel))
 }
 
 export { createWeeklyChart }
